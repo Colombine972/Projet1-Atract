9e0d7474969e6d80e8fe782c2fe01f5180a45f3c
Solomon Kalandadze <solokalandadze@gmail.com>
2025-10-16 15:39:03 +0200

    Added dinamyc modal functionality

--- modal.js
+++ modal.js
@@ -1,24 +1,70 @@
 const cards = document.querySelectorAll('.card')
 const modal = document.querySelector('.modal')
-const closeBtn = document.querySelector('#close_btn')
+let dynamicModal = document.querySelector('#dynamic_modal')
+const closeBtn = document.querySelector('.close_btn')
 const overlay = document.querySelector('.modal_overlay')
 
+// cards.forEach(card => {
+//   card.addEventListener('click', index => {
+//     modal.classList.add('active')
+//     overlay.classList.add('overlay_active')
+//     console.log(`You Clicked ${card.id}`)
+//   })
+// })
+
+
+
+//   closeBtn.addEventListener('click', () => {
+//     modal.classList.remove('active')
+//     overlay.classList.remove('overlay_active')
+//   })
+
+//   overlay.addEventListener('click', () => {
+//     modal.classList.remove('active')
+//     overlay.classList.remove('overlay_active')
+//   })
+
+
+// alternative modal opening 
+
 cards.forEach(card => {
-  card.addEventListener('click', index => {
-    modal.classList.add('active')
-    overlay.classList.add('overlay_active')
+  card.addEventListener('click', indx => {
+    let cardId = card.id;
+    openModal(cardId)
   })
 })
 
+function openModal(id) {
+  if(id == '1') {
+    modal.classList.add('active')
+    overlay.classList.add('overlay_active')
+  } else {
+    createModal(id)
+  }
+}
+
+function createModal (id) {
+  // let dynamicModal = document.querySelector('#dynamic_modal')
+  dynamicModal.innerHTML = ""
+  let title = document.createElement('h2')
+  let why = document.createElement('p')
+  title.innerText = `You Opened Modal Placeholder n${id}`
+  why.innerText = 'The content will be displayed dynamicaly'
+  dynamicModal.appendChild(title);
+  dynamicModal.appendChild(why)
+  dynamicModal.classList.add('active')
+  overlay.classList.add('overlay_active')
+}
+
 
   closeBtn.addEventListener('click', () => {
-    modal.classList.remove('active')
-    overlay.classList.remove('overlay_active')
-  })
+     modal.classList.remove('active')
+     dynamicModal.classList.remove('active')
+     overlay.classList.remove('overlay_active')
+   })
 
-  overlay.addEventListener('click', () => {
-    modal.classList.remove('active')
-    overlay.classList.remove('overlay_active')
-  })
-
-
+   overlay.addEventListener('click', () => {
+     modal.classList.remove('active')
+     dynamicModal.classList.remove('active')
+     overlay.classList.remove('overlay_active')
+   })
